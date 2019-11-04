bf4f2a35ab9a60702178d0c1f1cd501d31ddcea9
Blake Dietz <blakedietz@gmail.com>
2019-11-03 23:06:16 -0700

    feat: LeWitt 33

--- src/Page/Plotting.elm
+++ src/Page/Plotting.elm
@@ -1,16 +1,18 @@
 module Page.Plotting exposing (Model, Msg, init, randomMatrix, subscriptions, toSession, update, view)
 
 import Array exposing (Array)
+import Basics as Math
 import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import List.Extra
+import PolylinearScale exposing (polylinearScale)
 import Random exposing (generate, int)
 import Random.Array
 import Round
 import Session exposing (Session)
 import Svg exposing (..)
-import Svg.Attributes exposing (d, height, stroke, viewBox, width, x, y)
+import Svg.Attributes exposing (cx, cy, d, dy, fill, height, pointerEvents, r, stroke, strokeWidth, textAnchor, viewBox, width, x, y)
 import Task
 
 
@@ -20,6 +22,7 @@ import Task
 
 type alias Model =
     { session : Session
+    , numSquares : Int
     , grid : Grid
     }
 
@@ -27,7 +30,8 @@ type alias Model =
 init : Session -> ( Model, Cmd Msg )
 init session =
     ( { session = session
-      , grid = Array.initialize 10 (\_ -> Array.fromList [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ])
+      , numSquares = 10
+      , grid = Array.initialize 10 (\_ -> Array.repeat 10 1)
       }
     , Task.succeed GenerateNewGrid |> Task.perform identity
     )
@@ -35,6 +39,133 @@ init session =
 
 
 -- VIEW
+
+
+type alias ChartDimensions =
+    { width : Int
+    , height : Int
+    , numSquares : Int
+    }
+
+
+chartDimensions =
+    { width = 500, height = 500, numSquares = 10 }
+
+
+scale =
+    polylinearScale [ ( 0, 500 ), ( 500, 0 ) ]
+
+
+value =
+    let
+        scaledValue =
+            scale 0
+
+        safeValue =
+            case scaledValue of
+                Nothing ->
+                    ""
+
+                Just v ->
+                    String.fromFloat v
+    in
+    safeValue
+
+
+type Line
+    = VerticalBlackLine
+    | HorizontalYellowLine
+    | DiagonalRightRedLine
+    | DiagonalLeftBlueLine
+    | NoLine
+
+
+yellowLine num =
+    if Math.modBy 2 num == 0 then
+        HorizontalYellowLine
+
+    else
+        NoLine
+
+
+blackLine num =
+    if Math.modBy 3 num == 0 then
+        VerticalBlackLine
+
+    else
+        NoLine
+
+
+blueLine num =
+    if Math.modBy 4 num == 0 then
+        DiagonalLeftBlueLine
+
+    else
+        NoLine
+
+
+redLine num =
+    if Math.modBy 5 num == 0 then
+        DiagonalRightRedLine
+
+    else
+        NoLine
+
+
+viewSquare : Point -> Float -> Int -> List (Svg Msg)
+viewSquare point size line =
+    let
+        rectDimensions =
+            { topLeft = { x = point.x, y = point.y }
+            , topRight = { x = point.x + size, y = point.y }
+            , bottomLeft = { x = point.x, y = point.y - size }
+            , bottomRight = { x = point.x + size, y = point.y - size }
+            , centerLeft = { x = point.x, y = point.y + size / 2 }
+            , centerRight = { x = point.x + size, y = point.y + size / 2 }
+            , centerBottom = { x = point.x + size / 2, y = point.y + size }
+            , centerTop = { x = point.x + size / 2, y = point.y }
+            , center = { x = point.x + size / 2, y = point.y + size / 2 }
+            }
+
+        lines lineEnum =
+            case lineEnum of
+                VerticalBlackLine ->
+                    svgLineView [ rectDimensions.centerBottom, rectDimensions.centerTop ] [ stroke "black" ]
+
+                HorizontalYellowLine ->
+                    svgLineView [ rectDimensions.centerLeft, rectDimensions.centerRight ] [ stroke "yellow" ]
+
+                DiagonalRightRedLine ->
+                    svgLineView [ rectDimensions.bottomLeft, rectDimensions.topRight ] [ stroke "red" ]
+
+                DiagonalLeftBlueLine ->
+                    svgLineView [ rectDimensions.bottomRight, rectDimensions.topLeft ] [ stroke "blue" ]
+
+                NoLine ->
+                    svgLineView [] []
+    in
+    [ rect
+        [ x <| String.fromFloat point.x
+        , y <| String.fromFloat point.y
+        , width <| String.fromFloat size
+        , height <| String.fromFloat size
+        , fill "none"
+        , stroke "black"
+        ]
+        []
+    , lines (blackLine line)
+    , lines (blueLine line)
+    , lines (redLine line)
+    , lines (yellowLine line)
+
+    --    , Svg.text_
+    --        [ pointerEvents "none"
+    --        , x <| String.fromFloat rectDimensions.center.x
+    --        , y <| String.fromFloat rectDimensions.center.y
+    --        , textAnchor "middle"
+    --        ]
+    --        [ Svg.tspan [] [ Svg.text <| String.fromInt line ] ]
+    ]
 
 
 view : Model -> { title : String, content : Html Msg }
@@ -45,13 +176,10 @@ view model =
             [ div [ class "container page" ]
                 [ div [ class "row" ]
                     [ div [ class "col-md-9" ] <|
-                        [ svg
-                            [ width "120", height "120", viewBox "0 0 120 120" ]
-                            [ svgLineView [ { x = 0, y = 0 }, { x = 100, y = 100 } ]
-                            ]
+                        [ viewPiece chartDimensions model.grid
                         , button [ onClick GenerateNewGrid ]
                             [ Html.text "Regenerate" ]
-                        , gridView model.grid
+                        , gridToTable model.grid
                         ]
                     ]
                 ]
@@ -63,8 +191,56 @@ view model =
 -- DEBUGGING
 
 
-gridView : Grid -> Html Msg
-gridView grid =
+viewPiece : { width : Float, height : Float, numSquares : Int } -> Grid -> Html Msg
+viewPiece dimensions grid =
+    let
+        xScale =
+            polylinearScale [ ( 0.0, 0.0 ), ( toFloat dimensions.numSquares, dimensions.width ) ]
+
+        yScale =
+            polylinearScale [ ( 0.0, 0.0 ), ( toFloat dimensions.numSquares, dimensions.height ) ]
+
+        pointValues maybePoints =
+            maybePoints
+                |> List.foldr
+                    (\{ x, y, gridValue } acc ->
+                        case ( x, y, gridValue ) of
+                            ( Just x1, Just y1, Just gridVal ) ->
+                                { x = x1, y = y1, gridValue = gridVal } :: acc
+
+                            ( _, _, _ ) ->
+                                acc
+                    )
+                    []
+
+        squarePoints : List { x : Float, y : Float, gridValue : Int }
+        squarePoints =
+            cartesian (List.range 0 dimensions.numSquares) (List.range 0 dimensions.numSquares)
+                |> List.map (\( x, y ) -> { x = xScale <| toFloat x, y = yScale <| toFloat y, gridValue = Array.get y grid |> Maybe.andThen (Array.get x) })
+                |> pointValues
+
+        squareHeight =
+            dimensions.height / toFloat dimensions.numSquares
+
+        squareWidth =
+            dimensions.width / toFloat dimensions.numSquares
+    in
+    svg
+        [ width <| String.fromInt chartDimensions.width
+        , height <| String.fromInt chartDimensions.height
+        , viewBox <| "0 0 " ++ String.fromInt chartDimensions.width ++ " " ++ String.fromInt chartDimensions.height
+        ]
+    <|
+        -- Flattening
+        List.foldr (++) []
+        <|
+            List.map
+                (\{ x, y, gridValue } -> viewSquare { x = x, y = y } squareHeight gridValue)
+                squarePoints
+
+
+gridToTable : Grid -> Html Msg
+gridToTable grid =
     let
         columnToTd : Array Int -> List (Html Msg)
         columnToTd column =
@@ -108,8 +284,8 @@ roundPoint val =
     Round.round 2 (val * 100 / 100)
 
 
-svgLineView : Points -> Html Msg
-svgLineView points =
+svgLineView : Points -> List (Html.Attribute Msg) -> Html Msg
+svgLineView points attributes =
     let
         reducer : Int -> Point -> String -> String
         reducer index { x, y } acc =
@@ -123,7 +299,12 @@ svgLineView points =
         lineString =
             List.Extra.indexedFoldl reducer "" points
     in
-    path [ d lineString, stroke "red" ] []
+    path (attributes ++ [ d lineString ]) []
+
+
+cartesian : List a -> List b -> List ( a, b )
+cartesian xs ys =
+    List.concatMap (\x -> List.map (\y -> ( x, y )) ys) xs
 
 
 
@@ -142,7 +323,7 @@ type Msg
 
 
 randomMatrix =
-    Random.Array.array 10 (Random.Array.array 10 (int 0 4))
+    Random.Array.array 10 (Random.Array.array 10 (int 0 500))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
