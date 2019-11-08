9c471a4a90b8a2ea9cb8a4c8564c193ae981f029
Blake Dietz <blakedietz@gmail.com>
2019-11-08 14:45:01 -0700

    feat(LeWitt): First pass of adding and removing squares

--- src/Page/Plotting.elm
+++ src/Page/Plotting.elm
@@ -18,8 +18,19 @@ import Task
 type alias Model =
     { session : Session
     , numSquares : Int
-    , grid : Grid
+    , grid : SolLewitt.Grid
     }
+
+
+
+-- MSG
+
+
+type Msg
+    = GenerateNewGrid Int
+    | NewGrid SolLewitt.Grid
+    | IncrementedNumSquares
+    | DecrementedNumSquares
 
 
 init : Session -> ( Model, Cmd Msg )
@@ -28,7 +39,7 @@ init session =
       , numSquares = 10
       , grid = Array.initialize 10 (\_ -> Array.repeat 10 1)
       }
-    , Task.succeed GenerateNewGrid |> Task.perform identity
+    , Task.succeed (GenerateNewGrid 10) |> Task.perform identity
     )
 
 
@@ -55,49 +66,74 @@ view model =
             [ div [ class "flex-grow mt-5" ]
                 [ div [ class "flex flex-col justify-center items-center" ]
                     [ SolLewitt.view
-                        chartDimensions
+                        { width = 500, height = 500, numSquares = model.numSquares }
                         model.grid
 
                     --                , gridToTable model.grid
                     ]
                 ]
             , div [ class "self-end flex flex-col h-screen w-1/6 p-5 bg-gray-200 border border-gray-400" ]
-                [ button
-                    [ class "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-                    , onClick GenerateNewGrid
+                [ blueButtonView
+                    [ onClick <| GenerateNewGrid model.numSquares ]
+                    "Create new grid"
+                , text "Number of squares"
+                , div [ class "flex flex-row mt-5" ]
+                    [ redButtonView [ onClick DecrementedNumSquares ] "-"
+                    , div [ class "self-center" ] [ text <| String.fromInt model.numSquares ]
+                    , greenButtonView [ onClick IncrementedNumSquares ] "+"
                     ]
-                    [ Html.text "Create a new grid" ]
                 ]
             ]
     }
 
 
+blueButtonView : List (Attribute msg) -> String -> Html msg
+blueButtonView attributes internalText =
+    button
+        (class "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" :: attributes)
+        [ Html.text internalText ]
 
--- VISUALIZATION RULES
+
+redButtonView : List (Attribute msg) -> String -> Html msg
+redButtonView attributes internalText =
+    button
+        (class "bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded" :: attributes)
+        [ Html.text internalText ]
+
+
+greenButtonView : List (Attribute msg) -> String -> Html msg
+greenButtonView attributes internalText =
+    button
+        (class "bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded" :: attributes)
+        [ Html.text internalText ]
+
+
+
 -- UPDATE
-
-
-type alias Grid =
-    Array (Array Int)
-
-
-type Msg
-    = GenerateNewGrid
-    | NewGrid Grid
-
-
-randomMatrix =
-    Random.Array.array 10 (Random.Array.array 10 (int 0 500))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GenerateNewGrid ->
-            ( model, generate NewGrid randomMatrix )
+        GenerateNewGrid numSquares ->
+            ( model, generate NewGrid <| randomMatrix numSquares )
 
         NewGrid grid ->
             ( { model | grid = grid }, Cmd.none )
+
+        IncrementedNumSquares ->
+            let
+                newNumSquares =
+                    model.numSquares + 1
+            in
+            ( { model | numSquares = newNumSquares }, generate NewGrid <| randomMatrix newNumSquares )
+
+        DecrementedNumSquares ->
+            let
+                newNumSquares =
+                    model.numSquares - 1
+            in
+            ( { model | numSquares = newNumSquares }, generate NewGrid <| randomMatrix newNumSquares )
 
 
 
@@ -107,3 +143,11 @@ update msg model =
 toSession : Model -> Session
 toSession model =
     model.session
+
+
+
+-- EFFECTS
+
+
+randomMatrix numSquares =
+    Random.Array.array numSquares (Random.Array.array numSquares (int 0 500))
